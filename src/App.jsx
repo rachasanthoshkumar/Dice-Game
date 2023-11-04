77a72c559373a7a34aa76def0c48b31fbdb8d80a
rachasanthoshkumar <rachasanthosh2309@gmail.com>
2023-11-04 12:49:47 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import StartGame from "./components/startGame"
+import StartGame from "./components/StartGame"
 import GamePlay from "./components/GamePlay"
 function App() {
   
